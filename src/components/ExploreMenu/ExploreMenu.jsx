0e2f08ac472ae795967f306bdb2b63b3e6e5b502
Tharushiwijesinghe <tharushiwijesinghe99@gmail.com>
2025-04-19 21:33:16 +0530

    update

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-const ExploreMenu =(category,setCategory)=> {
+const ExploreMenu =({category,setCategory})=> {
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
@@ -10,8 +11,8 @@ const ExploreMenu =(category,setCategory)=> {
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
           return(
-            <div onClick={()=>setCategory(prev => prev===item.menu_name? "All":item.menu_name)} className={`explore-menu-item ${category===item.menu_name?"active":""}`} key={index}>
-              <img src={item.menu_image} alt=''/>
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name? "All":item.menu_name)} className="explore-menu-item" key={index}>
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=''/>
               <p>{item.menu_name}</p>
             </div>
           )
@@ -21,5 +22,6 @@ const ExploreMenu =(category,setCategory)=> {
     </div>
   )
 }
+
 
 export default ExploreMenu
