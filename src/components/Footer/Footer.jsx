f23dacb82e2f126a43a527356a4c85b32b460a52
Tharushiwijesinghe <tharushiwijesinghe99@gmail.com>
2025-05-09 10:59:55 +0530

    update footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className='footer-content'>
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" className='logo' />
+          <p>A Tomato is a mobile or web application that allows users to order food from local restaurants and have it delivered to their location. Users can browse menus, place orders, make payments online, and track the delivery in real time.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+94 77 456 8674</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved </p>
+      
+      </div>
+  )
+}
+
+export default Footer
