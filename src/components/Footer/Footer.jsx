e6ed6f79147216d2c963e74066abf91d62ebf747
Tharushiwijesinghe <tharushiwijesinghe99@gmail.com>
2025-05-09 08:38:47 +0530

    update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className='footer-content'>
+        <div className="footer-content-left">
+
+        </div>
+        <div className="footer-content-center">
+          
+        </div>
+        <div className="footer-content-right">
+
+        </div>
+      </div>
+
+
+
+      
+      </div>
+  )
+}
+
+export default Footer
