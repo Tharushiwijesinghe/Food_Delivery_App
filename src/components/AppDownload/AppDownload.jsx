5f8c11e24f5347bba1390a7e43b4751f59341542
Tharushiwijesinghe <tharushiwijesinghe99@gmail.com>
2025-05-10 09:52:29 +0530

    update responsiveness

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div>AppDownload</div>
+    <div className='app-download' id='app-download'>
+      <p>For Better Experience Download  <br /> Tomato App</p>
+      <div className="app-download-platforms">
+        <img src={assets.app_store} alt="" />
+        <img src={assets.play_store} alt="" />
+      </div>
+    </div>
   )
 }
 
